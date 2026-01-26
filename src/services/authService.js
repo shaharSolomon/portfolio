b0b0fb91f80143420439ff9dbbd8774f4e6d4cb5
shaharSolomon <nahmans12@gmail.com>
2026-01-26 21:37:10 +0200

    Delete src directory

--- src/services/authService.js
+++
@@ -1,9 +0,0 @@
-import { api } from "./api";
-
-export async function registerUser({ username, password }) {
-    return api.post("/auth/register", { username, password });
-}
-
-export async function loginUser({ username, password }) {
-    return api.post("/auth/login", { username, password });
-}
